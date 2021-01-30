560c92a3fec59c650018b993a3109c1b00412029
Gillian Yue <gillianyue1999@hotmail.com>
2021-01-30 20:18:18 +0800

    anim

---
+++ MemoryGame/Assets/Scripts/interactable.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class interactable : MonoBehaviour, IPointerClickHandler
+{
+    private int timesClicked = 0; //this should keep track of effective clicks
+    bool clickable = false;
+    Animator myAnimator;
+    int numInteractions;
+
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+
+        //test
+        clickable = true;
+        numInteractions = 4;
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print("clicked: " + eventData.pointerPress.name);
+        if (clickable)
+        {
+            timesClicked += 1;
+            checkBehavior();
+        }
+    }
+
+    public void fadeIn()
+    {
+        myAnimator.SetTrigger("fadeIn");
+    }
+
+    public void fadeOut()
+    {
+        myAnimator.SetTrigger("fadeOut");
+    }
+
+    //should be overidden by children?
+    public void checkBehavior()
+    {
+        if(timesClicked <= numInteractions)
+        {
+            print("checking behavior: " + timesClicked);
+            myAnimator.SetTrigger("action" + timesClicked.ToString());
+        }
+    }
+
+    public void setClickableTrue() { clickable = true; print("actually being set"); }
+
+    public void setClickableFalse() { clickable = false; }
+}
--- MemoryGame/Assets/Scripts/maskAdder.cs
+++ MemoryGame/Assets/Scripts/maskAdder.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class maskAdder : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //each state has two
+    public Sprite[] stateImgs;
+
+    private Image img1, img2;
+
     void Start()
+    {
+        img1 = transform.GetChild(0).GetComponent<Image>();
+        img2 = transform.GetChild(1).GetComponent<Image>();
+    }
+
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void switchToImgState(int s)
     {
-        
+        int idx1 = 2 * s, idx2 = 2 * s + 1;
+
+        img1.sprite = stateImgs[idx1];
+        img2.sprite = stateImgs[idx2];
     }
 }
