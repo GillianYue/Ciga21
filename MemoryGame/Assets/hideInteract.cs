a2337140c036de18a2ee85de244d2ce1fb36fb77
Gillian Yue <gillianyue1999@hotmail.com>
2021-06-17 00:51:20 +0800

    import fix

---
+++ MemoryGame/Assets/hideInteract.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hideInteract : MonoBehaviour
+{
+    Animator myAnimator;
+    public float distanceThreshold;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
